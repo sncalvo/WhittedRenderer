d7279b343ddacd9b3f6e023388c97f5de5b9514a
Santiago Calvo <sncalvo@hotmail.com>
2021-06-19 19:28:20 -0300

    Add face mesh

---
+++ src/Solids/Mesh.cpp
@@ -0,0 +1,81 @@
+#include "Mesh.hpp"
+
+#include <optional>
+
+#include <glm/gtc/constants.hpp>
+
+Mesh::Mesh(
+    std::vector<Face> faces,
+    glm::vec3 position,
+    Material material
+) :
+    _faces(faces),
+    _position(position),
+    Solid(material)
+{
+}
+
+std::optional<RayHit> Mesh::intersect(const Ray& ray)
+{
+    std::optional<Face> intersectedFace;
+    float tIntersection = -1.f;
+
+    for (Face face : _faces)
+    {
+        // Möller–Trumbore ray-triangle intersection algorithm
+        // https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+        glm::vec3 edge1 = face.p1 - face.p0;
+        glm::vec3 edge2 = face.p2 - face.p0;
+        glm::vec3 h = glm::cross(ray.direction, edge2);
+        float a = glm::dot(edge1, h);
+
+        if (a > -glm::epsilon<float>() && a < glm::epsilon<float>())
+        {
+            continue;
+        }
+
+        float f = 1.f / a;
+        glm::vec3 s = ray.origin - (_position + face.p0);
+        float u = f * glm::dot(s, h);
+
+        if (u < 0.f || u > 1.f)
+        {
+            continue;
+        }
+
+        glm::vec3 q = glm::cross(s, edge1);
+        float v = f * glm::dot(ray.direction, q);
+        if (v < 0.f || u + v > 1.f)
+        {
+            continue;
+        }
+
+        float t = f * glm::dot(edge2, q);
+ 
+        if (t > glm::epsilon<float>() && (tIntersection <= glm::epsilon<float>() || t < tIntersection))
+        {
+            tIntersection = t;
+            intersectedFace = face;
+        }
+    }
+
+    if (auto faceIntersected = intersectedFace)
+    {
+        glm::vec3 intersection = ray.origin + ray.direction * tIntersection;
+
+        glm::vec3 normal = glm::normalize(-glm::cross(
+            (faceIntersected->p0 - faceIntersected->p1),
+            (faceIntersected->p2 - faceIntersected->p0)
+        ));
+
+        auto isFrontFace = glm::dot(ray.direction, normal) < 0.f;
+        return RayHit{ intersection, normal, shared_from_this(), tIntersection, isFrontFace };
+    }
+
+    return {};
+}
+
+glm::vec3 Mesh::calculateNormal(glm::vec3 point) const
+{
+    return {};
+}
