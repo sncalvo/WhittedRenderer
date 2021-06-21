46408a316c6461e33b15e3657effae50177c205e
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2021-06-21 17:29:49 -0300

    auxiliary images (#10)
    * Use glm's linearRand function
    
    * Add auxiliary images for showing reflection and transparency
    
    * Remove debugging code

--- src/Solids/Plane.hpp
+++ src/Solids/Plane.hpp
@@ -9,6 +9,6 @@ private:
 	glm::vec3 _normal;
 public:
 	Plane(glm::vec3 center, glm::vec3 normal, Material material);
-	std::optional<RayHit> intersect(const Ray& ray);
-	glm::vec3 calculateNormal(glm::vec3 point) const;
+	std::optional<RayHit> intersect(const Ray& ray) override;
+	glm::vec3 calculateNormal(glm::vec3 point) const override;
 };
